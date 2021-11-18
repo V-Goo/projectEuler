0b47f7c979dab55355aff26ffd2e9a20ba343229
V-Goo <v-go@ya.ru>
2021-11-19 01:02:56 +0300

    start problem-1

---
+++ problem-1_(Multiples of 3 or 5)/problen-1_solution.js
@@ -0,0 +1,10 @@
+const limit = 10
+const arr = []
+console.log('arr: ', arr);
+
+function multiples(number, divider) {
+	if (number%divider !== 0) {
+		return false
+	}
+}
+console.log('multiples: ', multiples(8, 3));
